6f9eecaa2125f3aeb9a38ada86e7e531789f70ff
Evgeniy Gusar <igusar@intropro.com>
2016-02-16 12:46:24 +0200

    Fixed tabs and strip size.

--- app/src/main/java/com/gusar/datingapp/fragment/ViewPagerFragment.java
+++ app/src/main/java/com/gusar/datingapp/fragment/ViewPagerFragment.java
@@ -42,86 +42,13 @@ public class ViewPagerFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
 
-
-//        tabHost = new FragmentTabHost(getActivity());
-//        tabHost.setup(getActivity(),getChildFragmentManager(),R.id.testtabhost1);
-//
-//        tabHost.addTab(tabHost.newTabSpec("tab_test1").setIndicator("Persons"), PersonsFragment.class, null);
-//        tabHost.addTab(tabHost.newTabSpec("tab_test2").setIndicator("Maps"), MapFragment.class, null);
-
         mViewPager = (ViewPager) view.findViewById(R.id.pager);
         adapter = new MyAdapter(getChildFragmentManager());
         mViewPager.setAdapter(adapter);
-//        mViewPager.setAdapter(new SamplePagerAdapter());
 
         mSlidingTabLayout = (SlidingTabLayout) view.findViewById(R.id.sliding_tabs);
+        mSlidingTabLayout.setDistributeEvenly(true);
         mSlidingTabLayout.setViewPager(mViewPager);
-    }
-
-
-    class SamplePagerAdapter extends PagerAdapter {
-
-        /**
-         * @return the number of pages to display
-         */
-        @Override
-        public int getCount() {
-            return 10;
-        }
-
-        /**
-         * @return true if the value returned from {@link #instantiateItem(ViewGroup, int)} is the
-         * same object as the {@link View} added to the {@link ViewPager}.
-         */
-        @Override
-        public boolean isViewFromObject(View view, Object o) {
-            return o == view;
-        }
-
-        // BEGIN_INCLUDE (pageradapter_getpagetitle)
-        /**
-         * Return the title of the item at {@code position}. This is important as what this method
-         * returns is what is displayed in the {@link SlidingTabLayout}.
-         * <p>
-         * Here we construct one using the position value, but for real application the title should
-         * refer to the item's contents.
-         */
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return "Item " + (position + 1);
-        }
-        // END_INCLUDE (pageradapter_getpagetitle)
-
-        /**
-         * Instantiate the {@link View} which should be displayed at {@code position}. Here we
-         * inflate a layout from the apps resources and then change the text view to signify the position.
-         */
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            // Inflate a new layout from our resources
-            View view = getActivity().getLayoutInflater().inflate(R.layout.pager_item,
-                    container, false);
-            // Add the newly created View to the ViewPager
-            container.addView(view);
-
-            // Retrieve a TextView from the inflated View, and update it's text
-            TextView title = (TextView) view.findViewById(R.id.item_title);
-            title.setText(String.valueOf(position + 1));
-
-
-            // Return the View
-            return view;
-        }
-
-        /**
-         * Destroy the item from the {@link ViewPager}. In our case this is simply removing the
-         * {@link View}.
-         */
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            container.removeView((View) object);
-        }
-
     }
 
     public static class MyAdapter extends FragmentPagerAdapter {
